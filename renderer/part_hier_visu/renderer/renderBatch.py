1f00dcbdea6a5c120c76e50e4aad44ecf0477368
Ruidong Wu <mooninrain@outlook.com>
2020-07-09 14:36:35 -0400

    Update renderBatch.py

--- renderer/part_hier_visu/renderer/renderBatch.py
+++ renderer/part_hier_visu/renderer/renderBatch.py
@@ -5,15 +5,6 @@
 # modelpath = sys.argv[6]
 # pngpath = sys.argv[7]
 
-<<<<<<< HEAD
-# # bpy.ops.import_scene.obj(filepath = modelpath)
-# # bpy.data.scenes['Scene'].render.filepath = pngpath
-# # print('rendering')
-# # bpy.ops.render.render( write_still=True)
-# # print('rendered')
-# # sys.exit(0) # exit python and blender
-# # print('exited')
-=======
 bpy.context.scene.render.engine = 'CYCLES'
 bpy.ops.import_scene.obj(filepath = modelpath)
 bpy.data.scenes['Scene'].render.filepath = pngpath
@@ -22,4 +13,3 @@ bpy.ops.render.render( write_still=True)
 print('rendered')
 sys.exit(0) # exit python and blender
 print('exited')
->>>>>>> b0e58997cb66bbe9b956b811cb5d8365ad0118b4
