824c5637362e1794eb5bef7797962459b76c8d93
Ruidong Wu <mooninrain@outlook.com>
2020-08-26 15:38:06 -0400

    Update data_test.py

--- renderer/part_hier_visu/data_test.py
+++ renderer/part_hier_visu/data_test.py
@@ -1,10 +1,11 @@
 import os
 import json
+import tqdm
 
 root = '/data/vision/billf/scratch/ruidongwu/data/data_v0'
 all_dirs = os.listdir(root)
 all_nums = []
-for _dir_ in all_dirs:
+for _dir_ in tqdm(all_dirs):
     _path_ = os.path.join(root, _dir_,'result.json')
     with open(_path_,'r') as r:
         data = json.load(r)
