c58dcf1dabb41916f158c9072dfa6e7839860046
Ruidong Wu <mooninrain@outlook.com>
2020-08-26 15:33:43 -0400

    Create data_test.py

---
+++ renderer/part_hier_visu/data_test.py
@@ -0,0 +1,12 @@
+import os
+
+root = '/data/vision/billf/scratch/ruidongwu/data/data_v0'
+all_dirs = os.listdir(root)
+all_nums = []
+for _dir_ in all_dirs:
+    _path_ = os.path.join(root, _dir_,'result.json')
+    with open(_path_,'r') as r:
+        data = json.load(r)
+        num = len(data[0]['children'])
+        all_nums.append(num)
+print(max(all_nums))
